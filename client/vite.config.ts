3fc4c5ba017a53f745d172e92990b093866dae29
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-11-22 15:03:56 -0300

    refactor:organizando habiente de depuração e estruturando rotas

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './src',
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: './src/index.html',
+    },
   }
 })
